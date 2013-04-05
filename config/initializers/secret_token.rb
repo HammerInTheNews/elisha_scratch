e2ad16a1d7dbecefd590a4ec5b2fd31b071c299b
aljudson <alanjudson@gmail.com>
2013-04-05 14:01:18 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElishaScratch::Application.config.secret_token = 'da1822ce6ff2b07270471e78b20c15f269ba9fba4f72e3c600a80493fed4f26ea68e3102b2e26893d01ff954032a2adef59ec7fabe991ef603812b0454de751b'
